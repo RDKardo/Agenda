e7e446aa6b167b950dbd8469489392e1881ab10f
Ricardo <rdkardo@gmail.com>
2019-05-26 22:32:05 -0500

    Archivos de proyecto Agenda
    - Form1.cs es la ventana principal y se agregan los numeros telefónicos.
    - FormUno en presentation, es donde se agregan las personas.

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Aga
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textNumero = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 59);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1044, 392);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.DataGridView1_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Agregar Persona";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(16, 545);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView2.MultiSelect = false;
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(427, 195);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 498);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tipo";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(226, 501);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Numero";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Celular",
+            "Casa"});
+            this.comboBox1.Location = new System.Drawing.Point(80, 498);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // textNumero
+            // 
+            this.textNumero.Location = new System.Drawing.Point(304, 498);
+            this.textNumero.Name = "textNumero";
+            this.textNumero.Size = new System.Drawing.Size(121, 22);
+            this.textNumero.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(483, 569);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 47);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Guardar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(478, 657);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 47);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Eliminar Numero";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1213, 770);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textNumero);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textNumero;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;   
+using System.Windows.Forms;
+using Aga.Models;
+using Aga.Presentation;
+
+namespace Aga
+{
+    public partial class Form1 : Form
+    {
+        GetId gi = new GetId();
+        GetIdNum idN = new GetIdNum();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class GetPersona
+        {
+            public int IdPersona { get; set; }
+            public string Nombre { get; set; }
+            public string ApellidoPaterno { get; set; }
+            public string ApellidoMaterno { get; set; }
+            public string Correo { get; set; }
+            public int Edad { get; set; }
+            public DateTime FechaNacimiento { get; set; }
+            public bool Activo { get; set; }
+        }
+        public class GetNumero
+        {
+            public int IdNumero { get; set; }
+            public string Numero { get; set; }
+            public int IdPersona { get; set; }
+            public int IdTipo { get; set; }
+        }
+
+        public class GetId
+        {
+            public int iPersona { get; set; }
+        }
+        public class GetIdNum
+        {
+            public int idNumber { get; set; }
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            using (AgaTestEntities1 db = new AgaTestEntities1())
+            {
+                try
+                {
+                    List<GetPersona> List = new List<GetPersona>();
+                    string query = "SELECT IdPersona,Nombre,ApellidoPaterno,ApellidoMaterno,Correo,Edad,FechaNacimiento,Activo FROM Persona";
+                    List<GetPersona> result = db.Database.SqlQuery<GetPersona>(query).ToList();              
+                    dataGridView1.DataSource = result.ToList();
+                    List<GetNumero> List1 = new List<GetNumero>();
+                    string query1 = "SELECT * FROM Numero";
+                    List<GetNumero> result1 = db.Database.SqlQuery<GetNumero>(query1).ToList();
+                    dataGridView2.DataSource = result1.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }   
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Presentation.FormUno agregar = new Presentation.FormUno();
+            agregar.ShowDialog();
+            Reload();
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Presentation.FormTelefono tel = new Presentation.FormTelefono();
+            tel.ShowDialog();
+        }
+        /// <summary>
+        /// Obtiene ID de la persona
+        /// </summary>
+        /// <returns></returns>
+        public object getIdPersona()
+        {
+            var Id1 = 0;
+            FormTelefono telefono = new FormTelefono();
+            try
+            {
+                var count = dataGridView1.SelectedRows.Count;
+                if(dataGridView1.SelectedRows.Count != 0)
+                {
+                    DataGridViewRow row = this.dataGridView1.SelectedRows[0];
+                    var obj = row.Cells["IdPersona"].Value;
+                    gi.iPersona = Id1 = Convert.ToInt32(obj);
+                }
+                return gi.iPersona;
+            }
+            catch (Exception ee)
+            {
+                Console.Write("error" + ee.Message);
+                return 0;
+            }
+
+        }
+        /// <summary>
+        /// Recarga tablas
+        /// </summary>
+        public void Reload()
+        {
+            using (AgaTestEntities1 db = new AgaTestEntities1())
+            {
+                try
+                {
+                    List<GetPersona> List = new List<GetPersona>();
+                    string query = "SELECT IdPersona,Nombre,ApellidoPaterno,ApellidoMaterno,Correo,Edad,FechaNacimiento,Activo FROM Persona";
+                    List<GetPersona> result = db.Database.SqlQuery<GetPersona>(query).ToList();
+                    dataGridView1.DataSource = result.ToList();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+        public void ReloadNumber()
+        {
+            using (AgaTestEntities1 db = new AgaTestEntities1())
+            {
+                try
+                {
+                    List<GetNumero> List = new List<GetNumero>();
+                    string query = "SELECT * FROM Numero";
+                    List<GetNumero> result = db.Database.SqlQuery<GetNumero>(query).ToList();
+                    dataGridView2.DataSource = result.ToList();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+        public void DataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            using (AgaTestEntities1 db = new AgaTestEntities1())
+            {
+                try
+                {
+                    getIdPersona();
+                    //Inserción de datos de la persona
+                    Numero numero1 = new Numero
+                    {
+                        IdPersona = gi.iPersona,
+                        Numero1 = textNumero.Text,
+                        IdTipo = comboBox1.SelectedIndex == 0 ? 1 : 2
+                    };
+                    db.Numero.Add(numero1);
+                    db.SaveChanges();
+                    MessageBox.Show("Numero ingresado");
+                    ReloadNumber();
+                    //this.Close();
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error al ingresar numero");
+                    throw;
+                }
+                finally
+                {
+                    textNumero.Text = String.Empty;
+                    // this.Close();
+                }
+            }
+        }
+        public object GetIdNumero()
+        {
+            try
+            {
+                var IdNumber = 0;
+                var count = dataGridView2.SelectedRows.Count;
+                if (dataGridView2.SelectedRows.Count != 0)
+                {
+                    DataGridViewRow row = this.dataGridView2.SelectedRows[0];
+                    var obj = row.Cells["IdNumero"].Value;
+                    idN.idNumber = IdNumber = Convert.ToInt32(obj);
+                }
+                return idN.idNumber;
+            }
+            catch (Exception ee)
+            {
+                Console.Write("error" + ee.Message);
+                return 0;
+            }
+        }
+        /// <summary>
+        /// Elimina numero de persona
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            using (AgaTestEntities1 db = new AgaTestEntities1())
+            {
+                try
+                {
+                    GetIdNumero();
+                    //Inserción de datos de la persona
+                    Numero delete = db.Numero.Find(idN.idNumber);
+                    db.Numero.Remove(delete);
+                    db.SaveChanges();
+                    MessageBox.Show("Datos borrados");
+                    this.Close();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Datos borrados");
+                    throw;
+                }
+
+            }
+
+        }
+    }
+}
